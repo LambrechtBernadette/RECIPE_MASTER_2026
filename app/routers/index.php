b24effda77c32078d1e0eba6da7868ba51f74a4b
Bernadette Lambrecht <Lambrecht.Bernadette@proton.me>
2026-05-21 16:11:01 +0200

    Route recipes.show

--- app/routers/index.php
+++ app/routers/index.php
@@ -4,9 +4,13 @@
 //CTRL: pagesController (composite)
 //ACTION: home
 //pas de use car il ne sera utilisé une fois
+if (issets($_GET['recipes'])):
+    include_once '../app/routers/recipes.php';
 
-
+else:
 include_once '../app/controllers/pagesController.php';
 
 \App\Controllers\PagesController\homeAction($conn);
+
+endif;
 
